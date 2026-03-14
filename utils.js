a5f71991b287fd7a6fc6e7c635b36fef43056255
rabby7120 <rabby712islam@gmail.com>
2026-03-14 06:51:24 +0600

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T00:51:36.813Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-14T00:51:41.148Z
+Commit: Fix memory leak in event handlers
