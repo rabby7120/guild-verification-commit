e6f954fa88c23695f6cbd64b85594a60341b7189
rabby7120 <rabby712islam@gmail.com>
2026-03-14 06:51:18 +0600

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T00:51:15.244Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T00:51:35.229Z
+Commit: Fix CSS styling bug on mobile devices
